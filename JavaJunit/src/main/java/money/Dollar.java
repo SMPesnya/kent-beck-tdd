b200ea21b8f2fe7e9b9cb8bbcaecd0a308fd50e4
Stanislav Pesnya <pspesnya@gmail.com>
2025-09-16 09:01:17 +0300

    feat(chapter6): закончил главу, написал код
    Refs: DP-10 #time 30m #to-do #comment закончил главу №6, остановился на странице 47

--- JavaJunit/src/main/java/money/Dollar.java
+++ JavaJunit/src/main/java/money/Dollar.java
@@ -1,15 +1,10 @@
 package money;
 
-public class Dollar {
+public class Dollar extends Money {
     public Dollar(int amount){
         this.amount = amount;
-    }
-    public boolean equals(Object object){
-        Dollar dollar = (Dollar)object;
-        return (amount == dollar.amount);
     }
     public Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
     }
-    private int amount;
 }
---
+++ JavaJunit/src/main/java/money/Money.java
@@ -0,0 +1,9 @@
+package money;
+
+public class Money {
+    public boolean equals(Object object){
+        Money money = (Money)object;
+        return (amount == money.amount);
+    }
+    protected int amount;
+}
